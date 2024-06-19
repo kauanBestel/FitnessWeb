7cb26e2c09708a577953c91ab0bddbab3e2d9980
KauanBestel <kauan.bestell@gmail.com>
2024-06-19 08:49:27 -0300

    added the carrosel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { FormsComponent } from './pages/forms/forms.component';
 import { DicasComponent } from './pages/dicas/dicas.component';
 import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarroselComponent } from './carrosel/carrosel.component';
 
 const routes: Routes = [
   { path: 'home', component: FormsComponent }, 
   { path: 'dicas', component: DicasComponent }, 
   { path: 'sobre-nos', component: SobreNosComponent }, 
-  {path: 'footer', component: FooterComponent}
+  {path: 'footer', component: FooterComponent},
+  {path: 'carrosel', component: CarroselComponent},
+
 ];
 
 @NgModule({
